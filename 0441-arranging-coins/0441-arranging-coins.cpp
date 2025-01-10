aeae7d5bc6bd40e1c1fddcd379b097e828599dcc
Yehia NourEldin <123937741+Yehia-Nour@users.noreply.github.com>
2025-01-10 17:13:36 +0200

    Time: 9 ms (6.55%), Space: 8.8 MB (25.98%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int i = 1;
+       while(n > 0)
+       {
+           n -= i;      
+           i++;
+           if(n < i)
+           break;
+          
+       }
+       return i - 1;
+    }
+};
