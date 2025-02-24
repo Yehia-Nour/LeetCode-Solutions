88972cf4ebc48babaa0a0f5d793db51f68ec4d26
Yehia NourEldin <123937741+Yehia-Nour@users.noreply.github.com>
2025-02-24 16:12:32 +0200

    Time: 29 ms (10.42%), Space: 23.6 MB (33.22%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -15,9 +15,11 @@ public:
 
         if (root->val > p->val && root->val > q->val) {
            return lowestCommonAncestor(root->left, p, q);
-        } else if (root->val < p->val && root->val < q->val) {
+        }
+        else if (root->val < p->val && root->val < q->val) {
             return lowestCommonAncestor(root->right, p, q);
-        }else
+        }
+        else
             return root;
     }
 };
