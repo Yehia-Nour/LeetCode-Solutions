71361f39bfc24892d60c56bd1d388d83f24b65e2
Yehia NourEldin <123937741+Yehia-Nour@users.noreply.github.com>
2025-02-24 14:56:42 +0200

    Time: 3 ms (59.67%), Space: 23.1 MB (24.84%) - LeetHub

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+    	return sortedArrayToBST(nums, 0, (int) nums.size() - 1);
+    }
+
+    TreeNode* sortedArrayToBST(vector<int>& values, int start, int end) {
+    	if (start > end)
+    		return nullptr;
+
+    	// The best root for N sorted numbers is their middle number
+    	// as N/2 numbers before and after. Keep applying recursively
+    	int mid = (start + end) / 2;
+    	TreeNode *root = new TreeNode(values[mid]);
+    	root->left = sortedArrayToBST(values, start, mid - 1);
+    	root->right = sortedArrayToBST(values, mid + 1, end);
+
+    	return root;
+    }
+};
