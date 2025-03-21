6d97c4950eb6596993705674973a1ae505e61a70
Yehia NourEldin <123937741+Yehia-Nour@users.noreply.github.com>
2025-03-21 21:24:37 +0200

    23 ms (17.19%) Medium 03-21-2025 1823-find-the-winner-of-the-circular-game

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        queue<int> que;
+
+        for (int i = 1; i <= n; ++i) {
+            que.push(i);
+        }
+
+        while (que.size() > 1) {
+            for (int i = 0; i < k - 1; ++i) {
+                que.push(que.front());
+                que.pop();
+            }
+            que.pop();
+        }
+
+        return que.front();
+    }
+};
