d910793d74657a748535cb3c9dc1ca4ce8dc2c7e
Yehia NourEldin <123937741+Yehia-Nour@users.noreply.github.com>
2025-04-26 02:04:06 +0300

    8 ms (12.41%) Easy 04-26-2025 0009-palindrome-number

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string str = to_string(x);
+        int l = 0;
+        int r = str.length() - 1;
+        while(l < r){
+            if(str[l] != str[r]){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
