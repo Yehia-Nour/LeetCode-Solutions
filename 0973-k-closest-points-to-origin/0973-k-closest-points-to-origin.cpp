357662523956c07dcbad65f577e307866b80f035
Yehia NourEldin <123937741+Yehia-Nour@users.noreply.github.com>
2025-05-13 18:53:03 +0300

    36 ms (72.53%) Medium 05-13-2025 0973-k-closest-points-to-origin

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue< pair<int, int>, vector<pair<int, int>>,
+         greater<pair<int, int>> > minHeap;
+
+         for (int i = 0; i < points.size(); ++i) {
+            minHeap.push({points[i][0] * points[i][0] + 
+                          points[i][1] * points[i][1],
+                          i
+            });
+         }
+
+        vector<vector<int>> res;
+        for (int i = 0; i < k; ++i) {
+            res.push_back(points[minHeap.top().second]);
+            minHeap.pop();
+        }
+
+        return res;
+    }
+};
