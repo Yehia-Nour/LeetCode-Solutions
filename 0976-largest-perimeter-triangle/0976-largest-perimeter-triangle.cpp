70ea73deab2322118b6e11a33e88ef7293feeacd
Yehia NourEldin <123937741+Yehia-Nour@users.noreply.github.com>
2025-06-04 17:38:38 +0300

    Easy 1 ms (82.31%) 06-04-2025 25.7 MB (13.24%)

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
-    bool is_triangle(int a, int b, int c) {
-        return a + b > c && a + c > b && b + c > a;
-    }
     int largestPerimeter(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        int a, b, c;
         for (int i = nums.size() - 3; i >= 0; --i) {
-            a = nums[i];
-            b = nums[i + 1];
-            c = nums[i + 2];
-            if (is_triangle(a, b, c)) return a + b + c;
+            if (nums[i] + nums[i + 1] > nums[i + 2])
+                return nums[i] + nums[i + 1] + nums[i + 2];
         }
-
         return 0;
     }
 };
