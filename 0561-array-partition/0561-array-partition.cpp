5181b4b68a4e1480ef307c29b98ac324f64b3309
Yehia NourEldin <123937741+Yehia-Nour@users.noreply.github.com>
2025-06-04 18:27:48 +0300

    Easy 17 ms (19.17%) 06-04-2025 32.1 MB (53.95%)

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -5,7 +5,7 @@ public:
 
         int res = 0;
         for (int i = 0; i < nums.size(); i += 2) {
-            res += min(nums[i], nums[i + 1]);
+            res += nums[i];
         }
 
         return res;
