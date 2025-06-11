223d24a549ec35fd6170d2d2a52e4d1e086943c9
Yehia NourEldin <123937741+Yehia-Nour@users.noreply.github.com>
2025-06-11 15:45:14 +0300

    Medium 16 ms (53.29%) 06-11-2025 30.2 MB (5.82%)

---
+++ 0475-heaters/0475-heaters.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int findClosest(vector<int>& arr, int target) {
+        int start = 0, end = arr.size() - 1;
+        int closest = arr[0];
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (abs(arr[mid] - target) < abs(closest - target)) {
+                closest = arr[mid];
+            } else if (abs(arr[mid] - target) == abs(closest - target)) {
+                closest = std::min(closest, arr[mid]);
+            }
+
+            if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return closest;
+    }
+
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        sort(heaters.begin(), heaters.end());
+
+        int ans = 0;
+        for (int i = 0; i < houses.size(); i++) {
+            int temp = findClosest(heaters, houses[i]);
+            int d = abs(temp - houses[i]);
+            if (ans < d) ans = d;
+        }
+
+        return ans;
+    }
+};
