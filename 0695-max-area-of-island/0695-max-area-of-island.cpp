208606094df441b0e0d9995e939649e5ae931daf
Yehia NourEldin <123937741+Yehia-Nour@users.noreply.github.com>
2025-06-21 15:30:35 +0300

    Medium 2 ms (58.64%) 06-21-2025 28.2 MB (57.33%)

---
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool is_valid(int r, int c, vector<vector<int>> &grid) {
+        if (r < 0 || r >= grid.size()) return false;
+        if (c < 0 || c >= grid[0].size()) return false;
+        return true;
+    }
+
+    int dfs(int r, int c, vector<vector<int>> &grid, vector<vector<bool>> &visited) {
+        if (!is_valid(r, c, grid) || !grid[r][c] || visited[r][c]) return 0;
+
+        visited[r][c] = true;
+        int res = 1;
+		int dr[] { -1, 0, 1, 0 };
+		int dc[] { 0, 1, 0, -1 };
+		for (int d = 0; d < 4; ++d)
+			res += dfs(r + dr[d], c + dc[d], grid, visited);
+        
+        return res;
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+		vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size()));
+
+        int res = 0, temp = 0;
+
+        for (int r = 0; r < grid.size(); r++) {
+            for (int c = 0; c < grid[0].size(); c++) {
+                temp = dfs(r, c, grid, visited);
+                if (res < temp) res = temp;
+            }
+        }
+
+        return res;
+    }
+};
