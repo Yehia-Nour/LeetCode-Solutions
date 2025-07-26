ef50efd04bf3a9479707ff414208fea2cdd7d7b5
Yehia NourEldin <123937741+Yehia-Nour@users.noreply.github.com>
2025-07-26 16:31:51 +0300

    Medium 19 ms (85.23%) 07-26-2025 12.5 MB (87.36%)

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,30 @@
+string str1, str2;
+
+const int MAX = 1000 + 1;
+int memory[MAX][MAX];
+
+int LCS(int i, int j) {
+	if (i == (int) str1.size() || j == (int) str2.size())
+		return 0;
+
+	auto &ret = memory[i][j];
+	if (ret != -1)
+		return ret;
+
+	if (str1[i] == str2[j])
+		return ret = 1 + LCS(i + 1, j + 1);
+
+	int choice1 = LCS(i + 1, j);
+	int choice2 = LCS(i, j + 1);
+	return ret = max(choice1, choice2);
+}
+
+class Solution {
+public:
+	int longestCommonSubsequence(string text1, string text2) {
+		str1 = text1, str2 = text2;
+
+		memset(memory, -1, sizeof(memory));
+		return LCS(0, 0);
+	}
+};
