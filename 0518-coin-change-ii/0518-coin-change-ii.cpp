a6a01de55f52e32a6778b779e15805fac4199110
Yehia NourEldin <123937741+Yehia-Nour@users.noreply.github.com>
2025-07-31 14:34:45 +0300

    Medium 19 ms (55.48%) 07-31-2025 15.6 MB (75.15%)

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,26 @@
+vector<int> inputCoins;
+const int MAX_N = 300;
+const int MAX_AMOUNT = 5000 + 1;
+const int INF = 1e9;
+
+int memory[MAX_N][MAX_AMOUNT];
+int dp(int i, int remain) {
+    if (remain == 0) return 1;
+    if (i == inputCoins.size() || remain < 0) return 0;
+
+    int &ret = memory[i][remain];
+    if (ret != -1) return ret;
+
+    int take = dp(i, remain - inputCoins[i]);
+    int skip = dp(i + 1, remain);
+
+    return ret = take + skip;
+}
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        inputCoins = coins;
+        memset(memory, -1, sizeof(memory));
+        return dp(0, amount);
+    }
+};
