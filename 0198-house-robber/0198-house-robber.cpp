3cfca89f5415b365cb603ed42c44aa2d78eb015a
Yehia NourEldin <123937741+Yehia-Nour@users.noreply.github.com>
2025-08-13 16:32:12 +0300

    Time: 0 ms (100%), Space: 10.8 MB (5.84%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,24 +1,21 @@
-vector<int> numList;
-const int MAX_N = 100;
-int memory[MAX_N];
-
 class Solution {
 public:
-    int dp(int i) {
-        if (i >= numList.size()) return 0;
-
-        int &ret = memory[i];
-        if (ret != -1) return ret;
-
-        int rob = numList[i] + dp(i + 2);
-        int skip = dp(i + 1);
-
-        return ret = max(rob, skip);
-    }
-
     int rob(vector<int>& nums) {
-        numList = nums;
-        memset(memory, -1, sizeof(memory));
-        return dp(0);
+        int n = nums.size();
+
+        if (n == 1) {
+            return nums[0];
+        }
+
+        vector<int> dp(n, 0);
+
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+
+        return dp[n - 1];        
     }
 };
