aaece47feb232ab6efc3524b652c9ea4fe66aa68
Yehia NourEldin <123937741+Yehia-Nour@users.noreply.github.com>
2025-08-22 16:23:43 +0300

    Time: 0 ms (100%), Space: 7.8 MB (73.32%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,20 @@
+const int MAX_N = 46;
+int memory[MAX_N];
+class Solution {
+public:
+    int dp(int n){
+        if (n == 0) return 1;
+        if (n == 1) return 1;
+
+        auto &ret = memory[n];
+        if (ret != -1) return ret;
+
+        return ret = dp(n - 1) + dp(n - 2);
+    }
+
+    int climbStairs(int n) {
+        memset(memory, -1, sizeof(memory));
+
+        return dp(n);
+    }
+};
