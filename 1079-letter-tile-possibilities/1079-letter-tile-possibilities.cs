0fc2bf37cf5ee641918d7785da2dc6dc88d6fe8c
Yehia NourEldin <123937741+Yehia-Nour@users.noreply.github.com>
2025-09-26 18:01:04 +0300

    Time: 8 ms (95.92%), Space: 38.9 MB (79.59%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cs
@@ -0,0 +1,39 @@
+public class Solution
+{
+    int _res = 0;
+    string _str;
+    int _n;
+
+    void Backtrack(int count, bool[] used)
+    {
+        for (int i = 0; i < _n; i++)
+        {
+            if (used[i])
+                continue;
+
+            if (i > 0 && _str[i] == _str[i - 1] && !used[i - 1])
+                continue;
+
+            used[i] = true;
+
+            _res++;   // أي تسلسل جديد نضيفه للنتيجة
+
+            Backtrack(count + 1, used);
+
+            used[i] = false;
+        }
+    }
+
+    public int NumTilePossibilities(string tiles)
+    {
+        char[] chars = tiles.ToCharArray();
+        Array.Sort(chars);
+        _str = new string(chars);
+        _n = tiles.Length;
+
+        bool[] used = new bool[_n];
+        Backtrack(0, used);
+
+        return _res;
+    }
+}
